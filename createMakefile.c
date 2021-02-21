f03e5d236ba711b1f4dc83dd4dd2b78fca785d56
bitzhengqj@163.com <qinjian.zheng@student.unsw.edu.au>
2021-02-21 13:32:43 +0800

    add: program for creating makefile for each chap

---
+++ createMakefile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define FOR_EACH for(int i = 1; i <= atoi(argv[2]); i++)
+#define NAMESIZE 100
+
+int main(int argc, char const *argv[])
+{
+    char filename[NAMESIZE];
+    strcat(filename, argv[1]);
+    strcat(filename, "/Makefile");
+    FILE *f = fopen(filename, "w");
+    fprintf(f, "CC = g++\nCCFLAGS = -Wall -Werror -std=c++11\n");
+    FOR_EACH {
+        fprintf(f, "ex%d: ex%d.o\n\t$(CC) -o $@ $^\n\n", i, i);
+    }
+    FOR_EACH {
+        fprintf(f, "ex%d.o: ex%d.cpp\n\t$(CC) $(CCFLAGS) -c $^\n\n", i, i);
+    }
+    fprintf(f, "clean:\n\trm -rf *.o *.dSYM ");
+    FOR_EACH {
+        fprintf(f, "ex%d ", i);
+    }
+    fprintf(f, "\n\n");
+    fprintf(f, "create:\n\tfor ((i=1;i<=%d;i++)); do touch ex$$i.cpp; \
+        echo \\/\\* $$i\\*\\/ \\\\n\\\\n#include \\\"std_lib_facilities.h\\\" > ex$$i.cpp;done\n", atoi(argv[2]));
+
+    return 0;
+}
